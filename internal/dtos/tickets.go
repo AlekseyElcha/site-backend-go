d29d654353769038e32b1caa80ae8ffb957b7aaa
aleksey <aleshus2007@gmail.com>
2026-09-22 21:17:45 +0300

    updated dtos

--- internal/dtos/tickets.go
+++ internal/dtos/tickets.go
@@ -6,22 +6,46 @@ import (
 )
 
 type TicketGetResponse struct {
-	ID          uuid.UUID                   `json:"id"`
-	UserID      uuid.UUID                   `json:"user_id"`
-	Name        string                      `json:"name"`
-	Address     string                      `json:"address"`
-	PhoneNumber string                      `json:"phone_number"`
-	CreatedAt   time.Time                   `json:"created_at"`
-	Question    string                      `json:"question"`
-	Status      string                      `json:"status"`
-	FilesIDs    []uuid.UUID                 `json:"files_ids"`
-	Answers     []TicketAnswersInfoResponse `json:"answers"`
+	ID            uuid.UUID                 `json:"id"`
+	UserID        uuid.UUID                 `json:"user_id"`
+	Name          string                    `json:"name"`
+	Email         string                    `json:"email"`
+	Address       string                    `json:"address"`
+	PhoneNumber   string                    `json:"phone_number"`
+	CreatedAt     time.Time                 `json:"created_at"`
+	Question      string                    `json:"question"`
+	Status        string                    `json:"status"`
+	FilesIDs      []uuid.UUID               `json:"files_ids"`
+	Answer        *TicketAnswerInfoResponse `json:"answer"`
+	ExtraMessages *[]TicketExtraMessageInfo `json:"extra_messages"` // TODO: пересмотреть
 }
 
-type TicketAnswersInfoResponse struct {
-	ID         uuid.UUID `json:"id"`
-	CreatedAt  time.Time `json:"created_at"`
-	AnswerText string    `json:"answer_text"`
+type TicketGeneralInfo struct {
+	ID          uuid.UUID   `json:"id"`
+	UserID      uuid.UUID   `json:"user_id"`
+	CreatedAt   time.Time   `json:"created_at"`
+	Name        string      `json:"name"`
+	Email       string      `json:"email"`
+	PhoneNumber string      `json:"phone_number"`
+	Address     string      `json:"address"`
+	Question    string      `json:"question"`
+	Files       []uuid.UUID `json:"files"`
+	Status      string      `json:"status"`
+}
+
+type TicketAnswerInfoResponse struct { // TODO: пересмотреть
+	ID          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	MessageText string    `json:"answer_text"`
+}
+
+type TicketExtraMessageInfo struct {
+	ID          uuid.UUID   `json:"id"`
+	TicketID    uuid.UUID   `json:"ticket_id"`
+	SenderID    uuid.UUID   `json:"sender_id"`
+	SenderRole  string      `json:"sender_role"`
+	MessageText string      `json:"message_text"`
+	FilesID     []uuid.UUID `json:"files_id"`
 }
 
 type TicketCreateRequest struct {
